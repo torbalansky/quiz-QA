7563df89935ae0f7c92b029f83fc4c521fc8daa0
torbalansky <torbalansky@gmail.com>
2024-11-30 18:08:51 +0200

    Fix typo in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Quiz from './components/QuizSTLC';
